6af970cabb1c9bf64cac3dce4510abcfc3349322
sliman <ssoverlord04@gmail.com>
2024-12-04 01:18:15 +0100

    add contact me

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-document.querySelector(".year").innerHTML = new Date().getFullYear();
+const contactLink = document.getElementById("contact-link");
+const close = document.getElementById("close");
+contactLink.addEventListener("click", () => {
+  const contact = document.getElementById("contact");
+  contact.classList.toggle("hidden");
+});
+
+close.addEventListener("click", () => {
+  const contact = document.getElementById("contact");
+  contact.classList.toggle("hidden");
+});
+
+const submit = document.getElementById("contact-submit");
+submit.classList.add("not-allowed");
+submit.disabled = true;
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+name.addEventListener("input", () => {
+  if (
+    name.checkValidity() &&
+    email.checkValidity() &&
+    message.checkValidity()
+  ) {
+    submit.classList.remove("not-allowed");
+    submit.disabled = false;
+  } else {
+    submit.classList.add("not-allowed");
+    submit.disabled = true;
+  }
+});
+email.addEventListener("input", () => {
+  if (
+    name.checkValidity() &&
+    email.checkValidity() &&
+    message.checkValidity()
+  ) {
+    submit.classList.remove("not-allowed");
+    submit.disabled = false;
+  } else {
+    submit.classList.add("not-allowed");
+    submit.disabled = true;
+  }
+});
+message.addEventListener("input", () => {
+  if (
+    name.checkValidity() &&
+    email.checkValidity() &&
+    message.checkValidity()
+  ) {
+    submit.classList.remove("not-allowed");
+    submit.disabled = false;
+  } else {
+    submit.classList.add("not-allowed");
+    submit.disabled = true;
+  }
+});
